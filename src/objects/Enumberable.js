adf9905755d0b9570ffda3210c8cf6448501f066
fedeghe <fedeghe@gmail.com>
2021-03-11 22:22:44 +0100

    just the copy, need rewrite

---
+++ src/objects/Enumberable.js
@@ -0,0 +1,248 @@
+var Enumerable = (function() {
+    function each(iterator, context) {
+        try {
+            this._each(iterator, context);
+        } catch (e) {
+            if (e != $break) throw e;
+        }
+        return this;
+    }
+
+    function eachSlice(number, iterator, context) {
+        var index = -number,
+            slices = [],
+            array = this.toArray();
+        if (number < 1) return array;
+        while ((index += number) < array.length)
+            slices.push(array.slice(index, index + number));
+        return slices.collect(iterator, context);
+    }
+
+    function all(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var result = true;
+        this.each(function(value, index) {
+            result = result && !!iterator.call(context, value, index, this);
+            if (!result) throw $break;
+        }, this);
+        return result;
+    }
+
+    function any(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var result = false;
+        this.each(function(value, index) {
+            if (result = !!iterator.call(context, value, index, this))
+                throw $break;
+        }, this);
+        return result;
+    }
+
+    function collect(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var results = [];
+        this.each(function(value, index) {
+            results.push(iterator.call(context, value, index, this));
+        }, this);
+        return results;
+    }
+
+    function detect(iterator, context) {
+        var result;
+        this.each(function(value, index) {
+            if (iterator.call(context, value, index, this)) {
+                result = value;
+                throw $break;
+            }
+        }, this);
+        return result;
+    }
+
+    function findAll(iterator, context) {
+        var results = [];
+        this.each(function(value, index) {
+            if (iterator.call(context, value, index, this))
+                results.push(value);
+        }, this);
+        return results;
+    }
+
+    function grep(filter, iterator, context) {
+        iterator = iterator || Prototype.K;
+        var results = [];
+
+        if (Object.isString(filter))
+            filter = new RegExp(RegExp.escape(filter));
+
+        this.each(function(value, index) {
+            if (filter.match(value))
+                results.push(iterator.call(context, value, index, this));
+        }, this);
+        return results;
+    }
+
+    function include(object) {
+        if (Object.isFunction(this.indexOf) && this.indexOf(object) != -1)
+            return true;
+
+        var found = false;
+        this.each(function(value) {
+            if (value == object) {
+                found = true;
+                throw $break;
+            }
+        });
+        return found;
+    }
+
+    function inGroupsOf(number, fillWith) {
+        fillWith = Object.isUndefined(fillWith) ? null : fillWith;
+        return this.eachSlice(number, function(slice) {
+            while (slice.length < number) slice.push(fillWith);
+            return slice;
+        });
+    }
+
+    function inject(memo, iterator, context) {
+        this.each(function(value, index) {
+            memo = iterator.call(context, memo, value, index, this);
+        }, this);
+        return memo;
+    }
+
+    function invoke(method) {
+        var args = $A(arguments).slice(1);
+        return this.map(function(value) {
+            return value[method].apply(value, args);
+        });
+    }
+
+    function max(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var result;
+        this.each(function(value, index) {
+            value = iterator.call(context, value, index, this);
+            if (result == null || value >= result)
+                result = value;
+        }, this);
+        return result;
+    }
+
+    function min(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var result;
+        this.each(function(value, index) {
+            value = iterator.call(context, value, index, this);
+            if (result == null || value < result)
+                result = value;
+        }, this);
+        return result;
+    }
+
+    function partition(iterator, context) {
+        iterator = iterator || Prototype.K;
+        var trues = [],
+            falses = [];
+        this.each(function(value, index) {
+            (iterator.call(context, value, index, this) ?
+                trues : falses).push(value);
+        }, this);
+        return [trues, falses];
+    }
+
+    function pluck(property) {
+        var results = [];
+        this.each(function(value) {
+            results.push(value[property]);
+        });
+        return results;
+    }
+
+    function reject(iterator, context) {
+        var results = [];
+        this.each(function(value, index) {
+            if (!iterator.call(context, value, index, this))
+                results.push(value);
+        }, this);
+        return results;
+    }
+
+    function sortBy(iterator, context) {
+        return this.map(function(value, index) {
+            return {
+                value: value,
+                criteria: iterator.call(context, value, index, this)
+            };
+        }, this).sort(function(left, right) {
+            var a = left.criteria,
+                b = right.criteria;
+            return a < b ? -1 : a > b ? 1 : 0;
+        }).pluck('value');
+    }
+
+    function toArray() {
+        return this.map(Prototype.K);
+    }
+
+    function zip() {
+        var iterator = Prototype.K,
+            args = $A(arguments);
+        if (Object.isFunction(args.last()))
+            iterator = args.pop();
+
+        var collections = [this].concat(args).map($A);
+        return this.map(function(value, index) {
+            return iterator(collections.pluck(index));
+        });
+    }
+
+    function size() {
+        return this.toArray().length;
+    }
+
+    function inspect() {
+        return '#<Enumerable:' + this.toArray().inspect() + '>';
+    }
+
+
+
+
+
+
+
+
+
+    return {
+        each: each,
+        eachSlice: eachSlice,
+        all: all,
+        every: all,
+        any: any,
+        some: any,
+        collect: collect,
+        map: collect,
+        detect: detect,
+        findAll: findAll,
+        select: findAll,
+        filter: findAll,
+        include: include,
+        member: include,
+        inGroupsOf: inGroupsOf,
+        inject: inject,
+        invoke: invoke,
+        max: max,
+        min: min,
+        partition: partition,
+        pluck: pluck,
+        reject: reject,
+        sortBy: sortBy,
+        toArray: toArray,
+        entries: toArray,
+        zip: zip,
+        size: size,
+        inspect: inspect,
+        find: detect
+    };
+})();
+
+export default Enumerable
--- src/objects/index.js
+++ src/objects/index.js
@@ -2,8 +2,10 @@ import Prototype from './Prototype'
 import Ajax from './Ajax.js'
 import Class from './Class.js'
 import Hash from './Hash.js'
+import Enumerable from './Enumerable.js'
 
 export default {
+    Enumerable,
     Prototype,
     Ajax,
     Class,
