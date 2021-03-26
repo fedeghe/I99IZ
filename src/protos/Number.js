ba90390bb2d80bf51765072e0d4a85912b197f16
fedeghe <fedeghe@gmail.com>
2021-03-26 21:09:00 +0100

    number

--- src/protos/Number.js
+++ src/protos/Number.js
@@ -5,16 +5,25 @@ export const toPaddedString = (number, length, radix) => {
     var str = number.toString(radix || 10);
     return _String.times('0', length - str.length) + str;
 }
-const abs = () => { /*placeholder*/ }
-const ceil = () => { /*placeholder*/ }
-const floor = () => { /*placeholder*/ }
-const round = () => { /*placeholder*/ }
-const succ = () => { /*placeholder*/ }
-const times = () => { /*placeholder*/ }
-const toColorPart = () => { /*placeholder*/ }
-
-
+const abs = n => Math.abs(n)
+const ceil = n => Math.ceil(n)
+const floor = n => Math.floor(n)
+const round = n => Math.round(n)
+const succ = n => n + 1
+const times = (n, interator, ctx = null) => {
+    for (var i = 0; i < n; i++) {
+        interator.call(ctx, i)
+    }
+}
+const toColorPart = n => toPaddedString(n, 2, 16)
 
 export default {
+    abs,
+    ceil,
+    floor,
+    round,
+    succ,
+    times,
+    toColorPart,
     toPaddedString
 }
