fb360a2d62672c41af1553f4b2ba3d26a452133d
fedeghe <fedeghe@gmail.com>
2021-03-26 20:56:56 +0100

    recovered

--- src/objects/Hash.js
+++ src/objects/Hash.js
@@ -3,20 +3,20 @@ import _Class from './Class'
 import _Enumerable from './Enumerable'
 
 console.log('class:', _Class)
-const Hash = { a: 3 }
-    // const Hash = _Class.create(_Enumerable, (function() {
-    //     const toObject = o => _Object.clone(o._object);
 
-//     function initialize(object) {
-//         this._object = _Object.isHash(object) ?
-//             toObject(object) :
-//             Object.clone(object);
-//     }
-//     return {
-//         initialize,
-//         toObject,
-//         toTemplateReplacements: toObject
-//     };
-// })());
+const Hash = _Class.create(_Enumerable, (function() {
+    const toObject = o => _Object.clone(o._object);
+
+    function initialize(object) {
+        this._object = _Object.isHash(object) ?
+            toObject(object) :
+            Object.clone(object);
+    }
+    return {
+        initialize,
+        toObject,
+        toTemplateReplacements: toObject
+    };
+})());
 
 export default Hash;
