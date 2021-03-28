a5320cc76ab3fa7429a704942e12195f22e83027
fedeghe <fedeghe@gmail.com>
2021-03-29 00:37:59 +0200

    a good part of it

--- src/protos/Object.js
+++ src/protos/Object.js
@@ -17,7 +17,7 @@
  * values
  */
 // import Hash from './../objects/Hash'
-import extend from './../core/shared'
+import { extend } from './../core/shared'
 import { IS_DONTENUM_BUGGY, DONT_ENUMS } from './../core/constants'
 import {
     _hasOwnProperty,
@@ -37,54 +37,53 @@ export const clone = function(object) {
 }
 
 
-
-export default (function() {
-
-    function keys(object) {
-        if (!isObject(object)) {
-            throw new TypeError();
-        }
-        return Object.keys(object)
+export const keys = object => {
+    if (!isObject(object)) {
+        throw new TypeError();
     }
+    return Object.keys(object)
+}
 
-    function values(object) {
-        var results = [];
-        for (var property in object)
-            results.push(object[property]);
-        return results;
-    }
+export const values = object => {
+    var results = [];
+    for (var property in object)
+        results.push(object[property]);
+    return results;
+}
 
-    // function isHash(object) {
-    //     return object instanceof Hash;
-    // }
-    // function toQueryString(object) {
-    //     return Hash(object).toQueryString();
-    // }
+// function isHash(object) {
+//     return object instanceof Hash;
+// }
+// function toQueryString(object) {
+//     return Hash(object).toQueryString();
+// }
 
-    function stringify(object) {
-        return JSON.stringify(object);
-    }
+export const stringify = object => {
+    return JSON.stringify(object);
+}
 
-    function toHTML(object) {
-        return object && object.toHTML ? object.toHTML() : String.interpret(object);
-    }
+export const toHTML = object => {
+    return object && object.toHTML ? object.toHTML() : String.interpret(object);
+}
 
-    return {
-        clone,
-        extend,
-        inspect: o => JSON.stringify(o, null, 2),
-        isArray,
-        // isHash,
-        isString,
-        isDate,
-        isUndefined,
-        isElement,
-        isFunction,
-        isNumber,
-        keys,
-        // toQueryString,
-        values,
-        toJSON: stringify,
-        toHTML,
-    }
-})();
+
+
+
+export default {
+    clone,
+    extend,
+    inspect: o => JSON.stringify(o, null, 2),
+    isArray,
+    isDate,
+    isElement,
+    isFunction,
+    // isHash,
+    isNumber,
+    isString,
+    isUndefined,
+    keys,
+    toHTML,
+    toJSON: stringify,
+    // toQueryString,
+    values,
+}
---
+++ test/proto/Object.test.js
@@ -0,0 +1,170 @@
+/**
+ * @jest-environment jsdom
+ */
+import { screen } from '@testing-library/dom'
+import _Object from './../../src/protos/Object'
+import _Class from './../../src/objects/Class'
+import _String from './../../src/protos/String'
+
+describe('prototype - Object', function() {
+    it('clone', () => {
+        const o = { a: 1, b: { c: 33 } },
+            oStr = JSON.stringify(o),
+            oc = _Object.clone(o),
+            ocStr = JSON.stringify(oc);
+        expect(oStr).toBe(ocStr)
+    });
+
+    it('extend', () => {
+        const o = { a: 1, b: { c: 33 } },
+            ext = { d: 3, f: 44, fn: n => n ** 2 },
+            extended = _Object.extend(o, ext);
+        expect(o.d).toBe(3)
+        expect(extended.d).toBe(3)
+        expect(extended.fn(3)).toBe(9)
+    });
+
+    it('inspect', () => {
+        const o = { d: 3, f: 44, fn: n => n ** 2 },
+            inspection = _Object.inspect(o),
+            parsed = JSON.parse(inspection);
+        expect(parsed.d).toBe(3)
+        expect(parsed.f).toBe(44)
+    });
+
+    it('isArray', () => {
+        const bench = {
+            positive: [
+                [],
+                new Array, [1, 2, 3],
+            ],
+            negative: [
+                1,
+                false,
+                () => {},
+                Symbol('foo')
+            ]
+        }
+        bench.positive.forEach(b => expect(_Object.isArray(b)).toBe(true))
+        bench.negative.forEach(b => expect(_Object.isArray(b)).toBe(false))
+    });
+
+    it('isDate', () => {
+        const bench = {
+            positive: [
+                new Date(),
+            ],
+            negative: [
+                '', new String(), 1, false, () => {}, {},
+                [], Symbol('hello')
+            ]
+        }
+        bench.positive.forEach(b => expect(_Object.isDate(b)).toBe(true))
+        bench.negative.forEach(b => expect(_Object.isDate(b)).toBe(false))
+    });
+
+    it('isElement', () => {
+        document.body.innerHTML = `
+            <span data-testid="not-empty">
+                <span data-testid="empty"></span>
+            </span>
+            <div id="visible" data-testid="visible">Visible Example</div>
+        `
+        const node = screen.queryByTestId('visible')
+        expect(_Object.isElement(node)).toBe(true)
+    });
+
+    it('isFunction', () => {
+        const bench = {
+            positive: [
+                () => {},
+                function() {},
+                new Function(''),
+                Date, Object, Symbol, expect, it,
+            ],
+            negative: [
+                '', new String(), 1, false, {},
+                [], Symbol('hello')
+            ]
+        }
+        bench.positive.forEach(b => expect(_Object.isFunction(b)).toBe(true))
+        bench.negative.forEach(b => expect(_Object.isFunction(b)).toBe(false))
+    });
+
+    it.skip('isHash', () => {});
+
+    it('isNumber', () => {
+        const bench = {
+            positive: [
+                1, Math.PI, Math.E, 42.34, 3E-231
+            ],
+            negative: [
+                '', new String(), false, {},
+                [], Symbol('hello')
+            ]
+        }
+        bench.positive.forEach(b => expect(_Object.isNumber(b)).toBe(true))
+        bench.negative.forEach(b => expect(_Object.isNumber(b)).toBe(false))
+    });
+
+    it('isString', () => {
+        const bench = {
+            positive: [
+                '', new String(), 'sadasd', ['a', 'b'].join(),
+                `${bench}`, (() => 'hei')()
+            ],
+            negative: [
+                1, false, () => {}, {},
+                [], Symbol('hello')
+            ]
+        }
+        bench.positive.forEach(b => expect(_Object.isString(b)).toBe(true))
+        bench.negative.forEach(b => expect(_Object.isString(b)).toBe(false))
+    });
+
+    it('isUndefined', () => {
+        const bench = {
+            negative: [
+                '', new String(), 1, false, () => {}, {},
+                [], Symbol('hello')
+            ]
+        }
+        bench.negative.forEach(b => expect(_Object.isUndefined(b)).toBe(false))
+
+        // to test the positive we need a function
+        const fn = aaa => _Object.isUndefined(aaa)
+        expect(fn()).toBe(true)
+    });
+    it('keys', () => {
+        const aaa = { a: 1, b: 2, c: 3, d: { e: 4 } },
+            k = _Object.keys(aaa)
+        expect(JSON.stringify(k)).toBe('["a","b","c","d"]')
+    });
+    it('toJSON', () => {
+        const aaa = { a: 1, b: 2, c: 3, d: { e: 4 } },
+            str = JSON.stringify(aaa);
+        expect(_Object.toJSON(aaa)).toBe(str)
+    });
+    it.skip('toHTML', () => {
+        var Bookmark = _Class.create({
+            initialize: function(name, url) {
+                this.name = name;
+                this.url = url;
+            },
+            toHTML: function() {
+                return _String.interpolate('<a href="#{url}">#{name}</a>', this);
+            }
+        });
+        var api = new Bookmark('Prototype API', 'http://prototypejs.org/api');
+        expect(_Object.toHTML(api)).toBe('<a href="http://prototypejs.org/api">Prototype API</a>')
+    });
+    it.skip('toQueryString', () => {
+        // needs Hash
+    });
+    it('values', () => {
+        const aaa = { a: 1, b: 2, c: 3, d: { e: 4 } },
+            k = _Object.values(aaa)
+        expect(JSON.stringify(k)).toBe('[1,2,3,{"e":4}]')
+    });
+
+});
