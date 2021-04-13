7e727c134f14e94c1b31950a3ab85d4f15d64dd2
fedeghe <fedeghe@gmail.com>
2021-04-13 08:39:23 +0200

    wrap

--- test/proto/Function.test.js
+++ test/proto/Function.test.js
@@ -11,4 +11,19 @@ describe('prototype - Function', function() {
         expect(_Function.argumentNames(aFunction)).toMatchObject(["foo", "boo", "too"])
     })
 
+    it('wrap', () => {
+        function aFunction(foo, boo, too) {
+            return `${foo} - ${boo} - ${too}`
+        }
+        var wrapped = _Function.wrap(aFunction, function(origFunction, a, b, c, beK) {
+            if (beK) return origFunction(a, b, c);
+            else {
+                var tmp = origFunction(a, b, c)
+                return tmp.toUpperCase()
+            }
+        })
+        expect(wrapped('a', 'b', 'c')).toBe('A - B - C')
+        expect(wrapped('a', 'b', 'c', true)).toBe('a - b - c')
+    })
+
 });
